0b191b875ec5fb74aae8c2bd7118752dd73ed5b0
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-19 21:57:31 +0000

    automatically backed up by learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -7,6 +7,21 @@ module Slugifiable
 
   module ClassMethods
     def find_by_slug(slug)
+      slug_name = slug.gsub(' ', '-')
+      self.where("lower(name) = ?", slug_name).first
+    end
+  end
+end
+
+module Slugifiable
+  module InstanceMethods
+    def slug
+      self.name.strip.downcase.gsub(/\W/, '-')
+    end
+  end
+
+  module ClassMethods
+    def find_by_slug(slug)
       slug_name = slug.gsub('-', ' ')
       self.where("lower(name) = ?", slug_name).first
     end
